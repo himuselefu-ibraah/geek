d2fd9d9479ec6df6a8fe305192c25a134c724137
himuselefu-ibraah <ibrahimchristopher004@gmail.com>
2025-02-03 13:13:46 +0300

    initial commit

---
+++ introduction.py
@@ -0,0 +1,5 @@
+#print is a method used to display a value in a console
+print("Ibrahim")
+print("Emobilis")
+print(39)
+print(100*2)
---
+++ variable.py
@@ -0,0 +1,23 @@
+#Variable is a container used to store a value
+number = 10
+firstname ="Ibraah"
+lastname ="eMobilis"
+language = "python"
+a = 10
+b = 15
+
+d = 10
+g = 15
+u =25
+
+
+
+
+print(number)
+print(firstname)
+print(lastname)
+print(firstname,lastname)
+print(language,"is a web programming language")
+print(a+b)
+#the sum of 10 and 15 is 25
+print("the sum of",d,"and",g,"is",25)
